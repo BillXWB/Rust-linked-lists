57a264dc0e73759bc6614f777915ac08597ac230
Kirin <1370302499@qq.com>
2021-07-06 17:59:05 +0800

    ✨Feat(2.4): 实现 `push`

--- src/first.rs
+++ src/first.rs
@@ -1,9 +1,19 @@
+use std::mem::replace;
+
 pub struct List {
     head: Link,
 }
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem,
+            next: replace(&mut self.head, Link::Empty),
+        };
+        self.head = Link::More(Box::new(new_node));
     }
 }
 
